73eaa78d844a1106adfc42733d48ff1b993edbdf
stank <stanko.demon@gmail.com>
2024-04-10 17:44:09 +0300

    Additional exception handling

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -1,31 +1,25 @@
 package Matrix;
 
-import java.util.Objects;
-
 public class Matrix {
-    private int sum;
-    public Matrix(){
-        sum = 0;
-    }
-
-    public int getSum() {
-        return sum;
-    }
 
     public void addition(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4){
+        int sum = 0;
+
+        if (array.length != 4){
             throw new MyArraySizeException();
         }
-
         for (int i = 0; i < 4; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException();
+            }
             for (int j = 0; j < 4; j++) {
-                if (isNumber(Objects.requireNonNull(array[i][j]))) {
-                    sum += Integer.parseInt(array[i][j]);
-                } else {
+                if (array[i][j] == null || !isNumber(array[i][j])) {
                     throw new MyArrayDataException(i,j);
                 }
+                sum += Integer.parseInt(array[i][j]);
             }
         }
+        System.out.println("Сумма матрицы: " + sum);
     }
 
     private static boolean isNumber(String str){
